ff7d164beff9c486f63a48f2d8afc7225b51f613
Lokitosi <andres.gamba2011@gmail.com>
2021-02-10 17:32:39 -0500

    subido completo

---
+++ Hotel/src/Logic/Type.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic.Main;
+
+/**
+ *
+ * @author andre
+ */
+public class Type {
+    private String k_idTipo;
+    private String n_descripcion;
+    private Double v_precio;
+    
+   public Type(){
+       k_idTipo = "";
+       n_descripcion = "";
+       v_precio = 0.0;
+   }
+
+    public String getK_idTipo() {
+        return k_idTipo;
+    }
+
+    public void setK_idTipo(String k_idTipo) {
+        this.k_idTipo = k_idTipo;
+    }
+
+    public String getN_descripcion() {
+        return n_descripcion;
+    }
+
+    public void setN_descripcion(String n_descripcion) {
+        this.n_descripcion = n_descripcion;
+    }
+
+    public Double getV_precio() {
+        return v_precio;
+    }
+
+    public void setV_precio(Double v_precio) {
+        this.v_precio = v_precio;
+    }
+   
+   
+}
