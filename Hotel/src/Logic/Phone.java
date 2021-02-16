022e1855e57760126493ee7ffecd1ad85dc229ab
Juliono2 <jasr1601000@gmail.com>
2021-02-16 18:08:41 -0500

    Correccion DDL, transformacion y PhoneDAO a medias :v

---
+++ Hotel/src/Logic/Phone.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author Julian Sanchez
+ */
+public class Phone {
+    
+    /*ATRIBUTOS*/
+    private int k_telefono;
+    
+    /*CONSTRUCTOR*/
+
+    public Phone() {
+        k_telefono = 0;
+    }
+    
+    /*GETTERS*/
+    public int getK_telefono() {
+        return k_telefono;
+    }
+    
+    /*SETTERS*/
+    public void setK_telefono(int k_telefono) {
+        this.k_telefono = k_telefono;
+    }
+}
