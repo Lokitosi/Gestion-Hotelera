87038e8337bbf9bb7d48cb0a688149eb63875af0
Lokitosi <andres.gamba2011@gmail.com>
2021-02-17 20:31:31 -0500

    Añadido gitignore con las cosas a ignorar

--- Hotel/src/Database/ServiceLocator.java
+++ Hotel/src/Database/ServiceLocator.java
@@ -17,10 +17,11 @@ public class ServiceLocator {
         try {
             String url = "jdbc:postgresql://localhost:5432/Hotel";
             String usuario = "postgres";
-            String password = "doki11122001";
+            String password = "password";
             Class.forName("org.postgresql.Driver").newInstance();
             connection = DriverManager.getConnection(url, usuario, password);
             connection.setAutoCommit(false);
+            System.out.println("LO LOgramos :)");
         } catch (Exception e) {
             throw new CaException("ServiceLocator", "ERROR_CONEXION_BD " + e);
         }
